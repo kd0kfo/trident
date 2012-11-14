28a8a951a62c4b83c65ff19d8cd4482de991a99d
David Coss <David.Coss@stjude.org>
2012-11-14 15:32:07 -0600

    Changed chromosome chopper to allow the user to overwrite files that may already exist.

--- src/python/trident/chromosome_chopper.py
+++ src/python/trident/chromosome_chopper.py
@@ -13,9 +13,24 @@ import re
 def error_out(msg):
     print(msg)
     exit(1)
+def create_outfile_name(prefix,file_counter):
+    return "%s-%d" % (prefix,file_counter)
+
+def outfile_exists(prefix,file_counter):
+    """
+    Indictates wither or not the file that would be created already exists.
+
+    @param prefix: File prefix
+    @type prefix: String
+    @param file_counter: Index of file being created
+    @type file_counter: int
+    @return: bool
+    """
+    from os.path import isfile
+    return isfile(create_outfile_name(prefix,file_counter))
 
 def open_outfile(prefix,file_counter):
-    return open("%s-%d" % (prefix,file_counter),"w")
+    return open(create_outfile_name(prefix,file_counter) ,"w")
 
 def create_header(old_header,chunksize,seq_size):
     import datetime
@@ -48,12 +63,15 @@ def create_header(old_header,chunksize,seq_size):
     header += "[%s.%s]" % (species[0][0],species[1])
     return header + "\n"
     
-def chopper(filename,prefix,chunk_size):
+def chopper(filename,prefix,chunk_size,overwrite = True):
     """
     Opens the specified file and breaks it into 'chunk_size' number of lines.
     The results are placed in files, using the filename scheme prefix-index,
     where index is a zero-indexed value listing its position in the
     original file.
+
+    @param overwrite: Indicates whether files should be overwritten. If False, the files are left unmodified.
+    @type overwrite: bool
     """
     file = open(filename,"r")
 
@@ -71,20 +89,37 @@ def chopper(filename,prefix,chunk_size):
 
     file_counter = 1
     seq_offset = 0
-    outfile = open_outfile(prefix,file_counter)
-    outfile.write(header % (file_counter,seq_offset))
-    file_counter += 1
 
+    # Even if overwrite is True and we are not writing
+    # to a file, we need to keep track of lines and file_counter 
+    # values. So we need to turn writing on and off for each 
+    # individual segment. This will be done using inhibit_writing
+    inhibit_writing = False
+    if not overwrite and outfile_exists(prefix,file_counter):
+        inhibit_writing = True
+
+    outfile = None
+    if not inhibit_writing:
+        outfile = open_outfile(prefix,file_counter)
+        outfile.write(header % (file_counter,seq_offset))
+
+    file_counter += 1
     counter = 1
+    
     for line in file:
         if counter > 0 and counter % chunk_size == 0:
-            outfile.write(line)
-            outfile.close()
-            outfile = open_outfile(prefix,file_counter)
-            outfile.write(header % (file_counter,seq_offset))
+            if not inhibit_writing:
+                outfile.write(line)
+                outfile.close()
+            if not overwrite and outfile_exists(prefix,file_counter):
+                inhibit_writing = True
+            if not inhibit_writing:
+                outfile = open_outfile(prefix,file_counter)
+                outfile.write(header % (file_counter,seq_offset))
             file_counter += 1
             counter = 0
-        outfile.write(line)
+        if not inhibit_writing:
+            outfile.write(line)
         seq_offset += len(line.strip())
         counter += 1
 
