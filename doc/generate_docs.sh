35a179740df423cd3c0fd56b697415db324b168a
David Coss <David.Coss@stjude.org>
2013-01-28 13:37:04 -0600

    Added Javadoc to generate_docs.sh

--- doc/generate_docs.sh
+++ doc/generate_docs.sh
@@ -6,3 +6,7 @@ doxygen Doxyfile
 echo
 echo Document Trident Python Class
 epydoc -o python_api --name trident ../src/python/trident
+
+echo
+echo Running Javadoc on Hadoop code
+javadoc -d java_api ../hadoop/*.java ../hadoop/TridentHadoop/*.java
