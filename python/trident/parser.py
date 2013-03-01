5ce66202053375b059f15f051f30e58bfebfcdfd
David Coss <David.Coss@stjude.org>
2013-03-01 14:58:39 -0600

    Clean up imports in module.

--- python/trident/parser.py
+++ python/trident/parser.py
@@ -6,8 +6,6 @@ This may run as a standalone script or as a module, trident.parser
 
 """
 
-import sys, os 
-
 # Keys to score lines, which begin with the '>' character and are delimited by commas
 # Pig column field: (query_id:chararray,ref_id:chararray,score:int,energy:double,query_coord:chararray,ref_coords:chararray,length:int,identity:chararray,similarity:chararray,query_seq:chararray,match_seq:chararray,ref_seq:chararray,orientation:chararray,match_type:chararray,base_type:chararray)
 score_keys = ['query_id','reference_id','score','energy','query_coords','ref_coords','length','identity','similarity','query_seq','match_seq','reference_seq','orientation','match_type', 'base_type','query_start', 'query_end', 'ref_start', 'ref_end']
@@ -22,7 +20,8 @@ def parser_usage():
     """
     Display usage of trident
     """
-    print("Usage: %s <filename>" % sys.argv[0])
+    from sys import argv
+    print("Usage: %s <filename>" % argv[0])
     print("Parses the output of trident")
 
 def score_dict_to_str(score):
@@ -84,13 +83,13 @@ def score_str_to_dict(line):
     score['query_end'] = int(tokens[1])
     return score # End of score_str_to_dict
 
-def parse_file(file):
+def parse_file(infile):
     """
     Prints score information to stdout.
 
     Parameters: File Object
     """
-    line = file.readline()
+    line = infile.readline()
     if not line:# EOF
         raise StopIteration
     line = line.strip()
@@ -130,6 +129,7 @@ class Parser:
 def main():
     from getopt import getopt
     from sys import argv
+    import os.path as OP
 
     (optlist,args) = getopt(argv[1:],'v',['version'])
     
@@ -148,7 +148,7 @@ def main():
         parser_usage()
         exit(1)
     filename = args[0]
-    if not os.path.isfile(filename):
+    if not OP.isfile(filename):
         print("\"%s\" is not a file\n" % filename)
         parser_usage()
         exit(1)
