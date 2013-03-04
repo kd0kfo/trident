8645fa3ce2e5d0edbb766a22a8d2718862840837
David Coss <David.Coss@stjude.org>
2013-03-04 13:25:42 -0600

    Factored out main code for mirna_chopper into its own script.
    Cleaned up mirna_chopper while testing changes.

--- python/trident/mirna_chopper.py
+++ python/trident/mirna_chopper.py
@@ -6,14 +6,6 @@ Requires an input sequence, an output file prefix and a
 chunk size (number of 70-character lines).
 """
 
-from sys import argv
-import re
-
-
-def error_out(msg):
-    print(msg)
-    exit(1)
-
 def chopper(filename,chunk_size,specific_species = None):
     """
     Takes a microrna fasta file and splits it into smaller files.
@@ -26,14 +18,15 @@ def chopper(filename,chunk_size,specific_species = None):
     @type specific_species: str
     @return: Number of files written
     """
-    file = open(filename,"r")
+    infile = open(filename,"r")
     output_file = None
     chunk_counter = 0
     file_counter = 0
 
     write_seq = True
 
-    for line in file.readlines():
+    for line in infile.readlines():
+        line = line.strip()
         if len(line) == 0:
             continue
         new_line = ""
@@ -50,7 +43,7 @@ def chopper(filename,chunk_size,specific_species = None):
             if chunk_counter == 0:
                 if output_file != None:
                     output_file.close()
-                filename = new_line[1:].split(' ')[0]
+                filename = new_line[1:].strip().split(' ')[0]
                 if chunk_size > 1:
                     filename += "_and_more"
                 filename = filename.replace('*','_star')
@@ -63,38 +56,10 @@ def chopper(filename,chunk_size,specific_species = None):
 
         if write_seq:
             output_file.write(new_line)
+            output_file.write("\n")
     #end of for loop
     if output_file != None:
         output_file.close()
 
     return file_counter
 
-def main():
-    from getopt import getopt
-    
-    (optlist, args) = getopt(argv[1:],'s:',['species'])
-
-    specific_species = None
-
-    for (opt, val) in optlist:
-        while opt[0] == '-':
-            opt = opt[1:]
-        if opt in ['s', 'species']:
-            specific_species = val
-        else:
-            print("Unknown option: '%s'" % opt)
-
-    if len(args) < 2:
-        error_out("Usage: %s <filename> <number of mirna's>" % argv[0])
-
-    filename = args[0]
-    mirna_count = int(args[1])
-    if mirna_count < 0:
-        error_out("Invalid size. Must be a positive integer")
-
-    num_files = chopper(filename,mirna_count,specific_species)
-    print("Divided %s into %d segments" % (filename, num_files))
-
-
-if __name__ == "__main__":
-    main()
