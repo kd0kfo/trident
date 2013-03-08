788cec39b02c051cdd1a18652edcee21d50f02dc
David Coss <David.Coss@stjude.org>
2013-03-08 12:59:23 -0600

    Factored out energy-score array loading function to make this more uniform.
    Changed limits for the distribution plot.

--- python/trident/__init__.py
+++ python/trident/__init__.py
@@ -5,3 +5,8 @@ class FastaError(Exception):
     Exception for errors in a fasta file.
     """
     pass
+
+
+def load_energy_score_data(fname, dtype=float, comments="""#""", delimiter="\t", converters=None, skiprows=1, usecols=None, unpack=False, ndmin=0):
+    from numpy import loadtxt
+    return loadtxt(fname, dtype, comments, delimiter, converters, skiprows, usecols, unpack, ndmin)
