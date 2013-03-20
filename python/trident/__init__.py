f17edef3c7a34b83d4b17a6158dbaab63574d311
David Coss <David.Coss@stjude.org>
2013-03-20 11:36:34 -0500

    Changed trident.load_energy_score_data to check for the CDF magic number and use pupynere if it is found.

--- python/trident/__init__.py
+++ python/trident/__init__.py
@@ -12,5 +12,15 @@ class FastaError(TridentException):
 
 
 def load_energy_score_data(fname, dtype=float, comments="""#""", delimiter="\t", converters=None, skiprows=1, usecols=None, unpack=False, ndmin=0):
-    from numpy import loadtxt
-    return loadtxt(fname, dtype, comments, delimiter, converters, skiprows, usecols, unpack, ndmin)
+    use_netcdf = False
+    with open(fname,"r") as infile:
+        magic_number = infile.read(3)
+        if magic_number == "CDF":
+            use_netcdf = True
+    if use_netcdf:
+        import pupynere as nc
+        f = nc.netcdf_file(fname,"r")
+        return f.variables['ranks'].data
+    else:
+        from numpy import loadtxt
+        return loadtxt(fname, dtype, comments, delimiter, converters, skiprows, usecols, unpack, ndmin)
