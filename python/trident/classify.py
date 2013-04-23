60626b187e6539ad6ca22b4cccf06572486133e1
David Coss <David.Coss@stjude.org>
2013-04-23 15:45:44 -0500

    Added documentation

--- python/trident/classify.py
+++ python/trident/classify.py
@@ -7,9 +7,15 @@ Requires Scipy.
 from trident import TridentException
 
 class InvalidInterpolator(TridentException):
+    """
+    Exception to be raised when an invalid interpolation function is used or provided.
+    """
     pass
 
 class InterpolatorNotDefined(TridentException):
+    """
+    Exception to be raised when an interpolation function is not defined.
+    """
     pass
 
 class Interpolator:
@@ -28,11 +34,20 @@ class Interpolator:
         raise InterpolatorNotDefined("Interpolation function is not defined.")
     
     def save(self,filename):
+        """
+        Saves an Interpolator object using the pickle module.
+        
+        @param filename: Output filename
+        @ptype filename: str
+        """
         import pickle
         pickle.dump(self,open(filename,"wb"))
         
 
 class LinearInterpolator(Interpolator):
+    """
+    Template interpolation class only uses the energy and score data.
+    """
     def __init__(self):
         self.funct = None
         pass
@@ -41,6 +56,9 @@ class LinearInterpolator(Interpolator):
         return self.funct(score['energy'],score['score'])
     
 class MultiSpeciesInterpolator(Interpolator):
+    """
+    Template interpolation class that maps the species abbreviation from the 'query_id' section of the score to its Interpolator class.
+    """
     def __init__(self):
         self.species_map = {}
         
@@ -52,6 +70,15 @@ class MultiSpeciesInterpolator(Interpolator):
     
 
 def create_linear_interpolator(infilename):
+    """
+    Reads a file and returns a LinearInterpolator object.
+    
+    Requires scipy
+    
+    @param infilename: Input file name
+    @ptype infile: str
+    @return: LinearInterpolator
+    """
     from trident import load_energy_score_data
     import scipy.interpolate as SI
     import numpy as np
@@ -65,5 +92,12 @@ def create_linear_interpolator(infilename):
     return retval
         
 def load(filename):
-        import pickle
-        return pickle.load(open(filename,"rb"))
+    """
+    Loads an interpolator object.
+    
+    @param filename: Input file name
+    @ptype filename: str
+    @return: Interpolator instance
+    """
+    import pickle
+    return pickle.load(open(filename,"rb"))
